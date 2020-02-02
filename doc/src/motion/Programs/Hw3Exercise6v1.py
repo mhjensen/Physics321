e43eb2b656e6a7b6f04009f0e5a0338f5a2bffb0
mhjensen <morten.hjorth-jensen@fys.uio.no>
2020-02-01 23:05:56 -0500

    test hw3

--- doc/src/motion/Programs/Hw3Exercise6v1.py
+++ doc/src/motion/Programs/Hw3Exercise6v1.py
@@ -36,12 +36,11 @@ mpl.rcParams['font.family'] = 'serif'
 
 
 g = 9.80655 #m/s^2
+# Note that we bake inn the mass in D
 D = 0.00245 #m/s
 DeltaT = 0.01
-R = 0.1
-k = 100.0
 #set up arrays 
-tfinal = 10
+tfinal = 1.4
 n = ceil(tfinal/DeltaT)
 # define scaling constant vT
 vT = sqrt(g/D)
@@ -66,11 +65,8 @@ xanalytic[0] = x[0]
 # Start integrating using Euler's method
 for i in range(n-1):
     # expression for acceleration
-    N = 0.0
-    if (y[i] < R):
-        N = k*(R-y[i])
-    ax[i] = -D*vx[i]*vx[i]
-    ay[i] = -g - D*vy[i]*abs(vy[i])+N
+    ax[i] = -D*vx[i]*abs(vx[i])
+    ay[i] = -g - D*vy[i]*abs(vy[i])
     # update velocity and position
     x[i+1] = x[i] + DeltaT*vx[i]
     vx[i+1] = vx[i] + DeltaT*ax[i]
@@ -80,15 +76,15 @@ for i in range(n-1):
     t[i+1] = t[i] + DeltaT
     yanalytic[i+1] = y[0]-(vT*vT/g)*log(cosh(g*t[i+1]/vT))
     xanalytic[i+1] = log(1.+t[i+1]*vx[0]*D)/D
-#    if ( y[i+1] < 0.0):
-#        break
+    if ( y[i+1] < 0.0):
+        break
 ay[n-1] = -g + D*vy[n-1]*vy[n-1]
 ax[n-1] = -D*vx[n-1]*vx[n-1]
 data = {'t[s]': t,
-        'y[m]': y,#-yanalytic,
+        'Relative error in y': abs((y-yanalytic)/yanalytic),
         'vy[m/s]': vy,
         'ay[m/s^2]': ay,
-        'x[m]': x-xanalytic,
+        'Relative error in x': abs((x-xanalytic)/xanalytic),
         'vx[m/s]': vx,
         'ax[m/s^2]': ax
 }
@@ -97,15 +93,30 @@ display(NewData)
 
 
 fig, axs = plt.subplots(3, 1)
-axs[0].plot(t, y)#, t, yanalytic)
+axs[0].plot(t, y)
 axs[0].set_xlim(0, tfinal)
-axs[0].set_ylabel('y and exact')
+axs[0].set_ylabel('y')
 axs[1].plot(t, vy)
 axs[1].set_ylabel('vy[m/s]')
 axs[2].plot(t, ay)
 axs[2].set_xlabel('time[s]')
 axs[2].set_ylabel('ay[m/s^2]')
 fig.tight_layout()
-save_fig("EulerIntegration")
+save_fig("YEulerIntegration")
 plt.show()
+
+
+fig, axs = plt.subplots(3, 1)
+axs[0].plot(t, x)
+axs[0].set_xlim(0, tfinal)
+axs[0].set_ylabel('x')
+axs[1].plot(t, vx)
+axs[1].set_ylabel('vx[m/s]')
+axs[2].plot(t, ax)
+axs[2].set_xlabel('time[s]')
+axs[2].set_ylabel('ax[m/s^2]')
+fig.tight_layout()
+save_fig("XEulerIntegration")
+plt.show()
+
 
