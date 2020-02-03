f3b1e3a5bf5293071d8c172062ce11c17a4617e9
mhjensen <morten.hjorth-jensen@fys.uio.no>
2020-02-02 23:04:16 -0500

    test

---
+++ doc/src/energyconserv/Programs/Hw3Exercise6v1.py
@@ -0,0 +1,111 @@
+# Exercise 6, hw3, brute force way with declaration of vx, vy, x and y
+# Common imports
+import numpy as np
+import pandas as pd
+from math import *
+import matplotlib.pyplot as plt
+import os
+
+# Where to save the figures and data files
+PROJECT_ROOT_DIR = "Results"
+FIGURE_ID = "Results/FigureFiles"
+DATA_ID = "DataFiles/"
+
+if not os.path.exists(PROJECT_ROOT_DIR):
+    os.mkdir(PROJECT_ROOT_DIR)
+
+if not os.path.exists(FIGURE_ID):
+    os.makedirs(FIGURE_ID)
+
+if not os.path.exists(DATA_ID):
+    os.makedirs(DATA_ID)
+
+def image_path(fig_id):
+    return os.path.join(FIGURE_ID, fig_id)
+
+def data_path(dat_id):
+    return os.path.join(DATA_ID, dat_id)
+
+def save_fig(fig_id):
+    plt.savefig(image_path(fig_id) + ".png", format='png')
+
+# Output file
+outfile = open(data_path("Eulerresults.dat"),'w')
+
+from pylab import plt, mpl
+plt.style.use('seaborn')
+mpl.rcParams['font.family'] = 'serif'
+
+
+g = 9.80655 #m/s^2
+# The mass and the drag constant D
+D = 0.00245 #mass/length   kg/m
+m = 0.2 #kg, mass of falling object
+DeltaT = 0.01
+#set up arrays 
+tfinal = 1.4
+# set up number of points for all variables
+n = ceil(tfinal/DeltaT)
+# define scaling constant vT used in analytical solution
+vT = sqrt(m*g/D)
+# set up arrays for t, a, v, and y and arrays for analytical results
+#brute force setting up of arrays for x and y, vx, vy, ax and ay
+t = np.zeros(n)
+vy = np.zeros(n)
+y = np.zeros(n)
+vx = np.zeros(n)
+x = np.zeros(n)
+xanalytic = np.zeros(n)
+yanalytic = np.zeros(n)
+# Initial conditions
+vx[0] = 10.0 #m/s
+vy[0] = 0.0  #m/s
+y[0] = 10.0 #m
+x[0] = 0.0 #m
+yanalytic[0] = y[0]
+xanalytic[0] = x[0]
+# Start integrating using Euler's method
+for i in range(n-1):
+    # expression for acceleration, note the absolute value and division by mass
+    ax = -D*vx[i]*abs(vx[i])/m
+    ay = -g - D*vy[i]*abs(vy[i])/m
+    # update velocity and position
+    vx[i+1] = vx[i] + DeltaT*ax
+    x[i+1] = x[i] + DeltaT*vx[i]
+    vy[i+1] = vy[i] + DeltaT*ay
+    y[i+1] = y[i] + DeltaT*vy[i]
+    # update time to next time step and compute analytical answer
+    t[i+1] = t[i] + DeltaT
+    yanalytic[i+1] = y[0]-(vT*vT/g)*log(cosh(g*t[i+1]/vT))+vy[0]*t[i+1]
+    xanalytic[i+1] = log(1.+t[i+1]*vx[0]*D)/D
+    if ( y[i+1] < 0.0):
+        break
+data = {'t[s]': t,
+        'Relative error in y': abs((y-yanalytic)/yanalytic),
+        'vy[m/s]': vy,
+        'Relative error in x': abs((x-xanalytic)/xanalytic),
+        'vx[m/s]': vx
+}
+NewData = pd.DataFrame(data)
+display(NewData)
+# save to file
+NewData.to_csv(outfile, index=False)
+#then plot
+fig, axs = plt.subplots(4, 1)
+axs[0].plot(t, y)
+axs[0].set_xlim(0, tfinal)
+axs[0].set_ylabel('y')
+axs[1].plot(t, vy)
+axs[1].set_ylabel('vy[m/s]')
+axs[1].set_xlabel('time[s]')
+axs[2].plot(t, x)
+axs[2].set_xlim(0, tfinal)
+axs[2].set_ylabel('x')
+axs[3].plot(t, vx)
+axs[3].set_ylabel('vx[m/s]')
+axs[3].set_xlabel('time[s]')
+fig.tight_layout()
+save_fig("EulerIntegration")
+plt.show()
+
+
---
+++ doc/src/energyconserv/Programs/VelocityVerlet.py
@@ -0,0 +1,93 @@
+# The Velocity-Verlet applied to exercise 7 of HW3
+# Here we have added a normal force from the ground
+# Common imports
+import numpy as np
+import pandas as pd
+from math import *
+import matplotlib.pyplot as plt
+import os
+
+# Where to save the figures and data files
+PROJECT_ROOT_DIR = "Results"
+FIGURE_ID = "Results/FigureFiles"
+DATA_ID = "DataFiles/"
+
+if not os.path.exists(PROJECT_ROOT_DIR):
+    os.mkdir(PROJECT_ROOT_DIR)
+
+if not os.path.exists(FIGURE_ID):
+    os.makedirs(FIGURE_ID)
+
+if not os.path.exists(DATA_ID):
+    os.makedirs(DATA_ID)
+
+def image_path(fig_id):
+    return os.path.join(FIGURE_ID, fig_id)
+
+def data_path(dat_id):
+    return os.path.join(DATA_ID, dat_id)
+
+def save_fig(fig_id):
+    plt.savefig(image_path(fig_id) + ".png", format='png')
+
+
+from pylab import plt, mpl
+plt.style.use('seaborn')
+mpl.rcParams['font.family'] = 'serif'
+
+# Define constants
+g = 9.80655 #in m/s^2
+D = 0.0245 # in mass/length, kg/m
+m = 0.2 # in kg
+R = 0.1 # in meters
+k = 1000.0 # in mass/time^2
+# Define Gravitational force as a vector in x and y, zero x component
+G = -m*g*np.array([0.0,1])
+DeltaT = 0.01
+#set up arrays 
+tfinal = 15.0
+n = ceil(tfinal/DeltaT)
+# set up arrays for t, v, and r, the latter contain the x and y comps
+t = np.zeros(n)
+v = np.zeros((n,2))
+r = np.zeros((n,2))
+# Initial conditions
+r0 = np.array([0.0,2.0])
+v0 = np.array([10.0,10.0])
+r[0] = r0
+v[0] = v0
+# Start integrating using the Velocity Verlet method
+for i in range(n-1):
+    # Set up forces, air resistance FD
+    if ( r[i,1] < R):
+        N = k*(R-r[i,1])*np.array([0,1])
+    else:
+        N = np.array([0,0])
+    vabs = sqrt(sum(v[i]*v[i]))
+    FD = -D*v[i]*vabs
+    Fnet = FD+G+N
+    a = Fnet/m
+    # update velocity, time and position
+    r[i+1] = r[i] + DeltaT*v[i+1]+DeltaT*DeltaT*0.5*a
+    # Calculate acceleration at time t+1
+    if ( r[i+1,1] < R):
+        N = k*(R-r[i+1,1])*np.array([0,1])
+    else:
+        N = np.array([0,0])
+    Fnet = FD+G+N        
+    anew = Fnet/m
+    v[i+1] = v[i] + 0.5*DeltaT*(a+anew)
+    t[i+1] = t[i] + DeltaT
+
+fig, ax = plt.subplots()
+ax.set_xlim(0, tfinal)
+ax.set_ylabel('y[m]')
+ax.set_xlabel('x[m]')
+ax.plot(r[:,0], r[:,1])
+fig.tight_layout()
+save_fig("BouncingBallEuler")
+plt.show()
+
+
+
+
