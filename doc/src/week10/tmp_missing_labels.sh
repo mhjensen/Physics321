22e59c33a14409728bf8314547493b7a932e2655
Morten Hjorth-Jensen <morten.hjorth-jensen@fys.uio.no>
2021-03-07 22:11:25 +0100

    Updated week 10

--- doc/src/week10/tmp_missing_labels.sh
+++
@@ -1,6 +0,0 @@
-#rm -rf tmp_mako__* tmp_preprocess__*
-find . -name '*.do.txt' -exec grep --color --with-filename --line-number "{eq:fastdriven1}" {} \;
-find . -name '*.do.txt' -exec grep --color --with-filename --line-number "{eq:fastdriven2}" {} \;
-find . -name '*.do.txt' -exec grep --color --with-filename --line-number "{eq:partform}" {} \;
-find . -name '*.do.txt' -exec grep --color --with-filename --line-number "{eq:Ddrive}" {} \;
-find . -name '*.do.txt' -exec grep --color --with-filename --line-number "{eq:homogsolution}" {} \;
