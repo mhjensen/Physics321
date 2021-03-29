4956fd2cd803187346178429b43588cdf1b80343
Morten Hjorth-Jensen <morten.hjorth-jensen@fys.uio.no>
2021-03-29 07:21:36 -0400

    updating

--- doc/src/week13/tmp_missing_labels.sh
+++
@@ -1,4 +0,0 @@
-#rm -rf tmp_mako__* tmp_preprocess__*
-find . -name '*.do.txt' -exec grep --color --with-filename --line-number "{eq:radialeqofmotion}" {} \;
-find . -name '*.do.txt' -exec grep --color --with-filename --line-number "{eq:Ctrajectory}" {} \;
-find . -name '*.do.txt' -exec grep --color --with-filename --line-number "{eq:Ctrajectory}" {} \;
